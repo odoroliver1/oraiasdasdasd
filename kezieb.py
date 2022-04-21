07ee72fb71448a5be2a666a5cfc7b284877baa4e
odoroliver1 <71767269+odoroliver1@users.noreply.github.com>
2022-04-21 09:38:37 +0200

    Add files via upload

---
+++ kezieb.py
@@ -0,0 +1,18 @@
+class Jatekos:
+    def __init__(self.egysor):
+        darabok.egysor.split(";")
+        self.nev=darabok[0]
+        self.dobottgol=int(darabok[1])
+        self.kapuraloves=int(darabok[2])
+        self.sikereshetes=int(darabok[3])
+        self.hetesprobalkozas=int(darabok[4])
+        self.ketperceskiallitas=int(darabok[5])
+        self.poszt=darabok[6]
+        self.magassag=int(darabok[7])
+        self.csapat=darabok[8]
+f=open("kezieb.txt", "r", encoding="utf8")
+beolvas=f.readlines()
+f.close()
+jatekosok=[]
+for item in beolvas:
+    (jatekosok.append[8].nev)
